c448d9d8d4e0f482309044b83302f4e2766b4416
Achraf Bouyakhsass <achraf.b@mutationevent.com>
2017-01-02 17:02:40 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,6 +17,14 @@ remove_action('wp_head', 'print_emoji_detection_script', 7 );
 remove_action('admin_print_scripts', 'print_emoji_detection_script' );
 remove_action('wp_print_styles', 'print_emoji_styles' );
 remove_action('admin_print_styles', 'print_emoji_styles' );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+add_filter( 'embed_oembed_discover', '__return_false' );
+remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
 
 
 /**
